d7eafd30ee7b26930e7193fda26d287b22d62bd7
erdonsu <37120175+erdonsu@users.noreply.github.com>
2026-01-28 16:10:29 +0700

    sssssssss

--- s.py
+++ s.py
@@ -40,8 +40,8 @@ image = (
         "pip install --upgrade pip",
         "pip install --no-cache-dir comfy-cli uv",
         "uv pip install --system --compile-bytecode huggingface_hub[hf_transfer]==0.28.1",
-        # Install ComfyUI to default location
-        "comfy --skip-prompt install --nvidia"
+        # Install ComfyUI from official repo
+        "git clone https://github.com/Comfy-Org/ComfyUI.git /root/comfy/ComfyUI && cd /root/comfy/ComfyUI && pip install -e ."
     ])
     .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
     # dependencies install for some nodes
